849d2fec70df16eccbd8edca3a31dd2bc38c0bd7
aaronchenghs <aaronchenghs@gmail.com>
2023-01-19 14:58:17 -0600

    Contact page

---
+++ src/routes/contactme/contactbox/ContactBox.component.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+import './ContactBox.styles.scss';
+
+const ContactBox = () => {
+    return (
+        <div className="Contact-Container">
+
+        </div>
+    )
+}
+export default ContactBox;
--- src/routes/contactme/contactme.component.jsx
+++ src/routes/contactme/contactme.component.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import ContactBox from "./contactbox/ContactBox.component";
 
 import './contactme.styles.scss';
 
@@ -6,9 +7,16 @@ const ContactMe = () => {
   return (
     <Fragment>
       <body className="ContactMe-Page">
-
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
+        <ContactBox></ContactBox>
       </body>
     </Fragment>
   )
 }
 export default ContactMe;
+ //email, phone, linkedin, github, gitlab, discord, instagram
