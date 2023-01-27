e91410ebf8fcf3666db930369e388d6b2764a119
Aaron Cheng <acheng8@lsu.edu>
2023-01-26 21:32:12 -0600

    Home progress

--- src/routes/home/introduction/introduction.component.jsx
+++ src/routes/home/introduction/introduction.component.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Fragment } from "react";
+import { introText, nameText } from "../categories";
 
 import "./introduction.styles.scss";
 
 const Introduction = () => {
   return (
     <Fragment>
-        <div className="image-container">
-          <img></img>
+      <div className="image-container">
+        <img src="https://i.ibb.co/JpYzrjC/profile.png" alt="profile" border="0" />
+      </div>
+      <div className="text-container">
+        <div className="name-text">
+        <strong>{nameText}</strong>
         </div>
-        <div className="text-container"></div>
-    </Fragment>
+        <div className="text">
+          {introText}
+        </div>
+      </div>
+    </Fragment >
   );
 };
 export default Introduction;
