d4db7c9a5016dcefe5b2361746bcbc3aafffab4f
aaronchenghs <aaronchenghs@gmail.com>
2023-02-02 12:12:20 -0600

    Added sample svg

--- src/routes/portfolio/categories.jsx
+++ src/routes/portfolio/categories.jsx
@@ -1,30 +1,34 @@
+import { ReactComponent as Sample } from "../../assets/sample.svg";
+const SampleHred =
+  "https://img.icons8.com/external-bearicons-blue-bearicons/64/null/external-SVG-file-extension-bearicons-blue-bearicons.png";
+
 export const projects = [
   {
     id: 1,
     title: "Elapsed",
     href: "",
-    imgPath: "",
+    imgPath: SampleHred,
     description: "This describes Elapsed",
   },
   {
     id: 2,
     title: "Exodus",
     href: "",
-    imgPath: "",
+    imgPath: SampleHred,
     description: "This describes Exodus",
   },
   {
     id: 3,
     title: "Task Remote",
     href: "",
-    imgPath: "",
+    imgPath: SampleHred,
     description: "This describes Task Remote",
   },
   {
     id: 4,
     title: "This Website!",
     href: "",
-    imgPath: "",
+    imgPath: SampleHred,
     description: "Click this to view this site's source code",
   },
 ];
