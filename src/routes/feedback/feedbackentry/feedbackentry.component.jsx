6710d5fb3cc3f4a31a7d0c85288e8f85d7d84b16
Aaron Cheng <acheng8@lsu.edu>
2023-03-16 22:19:25 -0500

    Fixed more styling smh

--- src/routes/feedback/feedbackentry/feedbackentry.component.jsx
+++ src/routes/feedback/feedbackentry/feedbackentry.component.jsx
@@ -3,8 +3,10 @@ import "./feedbackentry.styles.scss";
 const FeedbackEntry = ({ name, message, date }) => {
   return (
     <div className="feedback-entry">
-      <label className="name-container">{name}</label>
-      <label className="date-container">on {date}</label>
+      <label className="name-container">
+        {name}
+        <label className="date-container">on {date}</label>
+      </label>
       <div className="message-container">{message}</div>
     </div>
   );
