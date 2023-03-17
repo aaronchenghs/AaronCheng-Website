b5fa0cabab47bacafb34a0743a5f7c8fa033c1ba
Aaron Cheng <acheng8@lsu.edu>
2023-03-16 21:16:14 -0500

    More styling adjustments

--- src/routes/portfolio/categories.jsx
+++ src/routes/portfolio/categories.jsx
@@ -49,7 +49,7 @@ export const projects = [
   {
     id: 4,
     title: "Akon",
-    href: "https://github.com/Andrivite/CSC-3380-Project/tree/main",
+    href: "https://github.com/aaronchenghs/Akon",
     imgPath: "https://i.ibb.co/HDQ5xX1/Akon.png",
     description: `Akon is an awesome music-playing and utilities bot I designed one winter, based on my
     favorite Senegalese singer. Click this title bar to get it's source code!`,
