707be921dfd9d2b4194c6e48a9fb784ee6a2a4e9
Aaron Cheng <acheng8@lsu.edu>
2023-03-19 17:34:16 -0500

    Started story tab

--- src/routes/vita/stats/categorycomponents/story.component.jsx
+++ src/routes/vita/stats/categorycomponents/story.component.jsx
@@ -1,8 +1,25 @@
 import React, { Fragment } from "react";
+import { story_svgs, story_text } from "./svgs/story.svgs";
 
 import "./story.styles.scss";
 
 export const Story = () => {
-  return <Fragment>Story</Fragment>;
+  return (
+    <Fragment>
+      <div className="story-logLine">{story_text.log}</div>
+      <div className="timeline-container">
+        <div className="timeline-line" />
+        <div className="geographic-origin-text-container">
+          <div className="parents">
+            <label style={{ "margin-right": "0.7vw" }}>
+              {story_svgs.US_flag}
+              {story_svgs.BR_flag}
+            </label>
+            {story_text.parents}
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 export default Story;
---
+++ src/routes/vita/stats/categorycomponents/svgs/story.svgs.jsx
@@ -0,0 +1,57 @@
+const flag_style = {
+  "object-fit": "cover",
+  height: "50px",
+  width: "75px",
+  "vertical-align": "middle",
+};
+
+export const story_svgs = {
+  BR_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg"
+      alt="BR flag"
+      style={flag_style}
+    />
+  ),
+  US_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg"
+      alt="US flag"
+      style={flag_style}
+    />
+  ),
+  IA_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/7/7d/Flag_of_Iowa_%28variant%29.svg"
+      alt="IA flag"
+      style={flag_style}
+    />
+  ),
+  HI_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/e/ef/Flag_of_Hawaii.svg"
+      alt="HI flag"
+      style={flag_style}
+    />
+  ),
+  FL_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg"
+      alt="FL flag"
+      style={flag_style}
+    />
+  ),
+  LA_flag: (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Flag_of_Louisiana.svg"
+      alt="LA flag"
+      style={flag_style}
+    />
+  ),
+};
+
+export const story_text = {
+  log: `Describing my background in-person is a 
+        tangled verbiage; here's my solution :-)`,
+  parents: `My parents immigrated from Brazil to the United States in the early 90's`,
+};
