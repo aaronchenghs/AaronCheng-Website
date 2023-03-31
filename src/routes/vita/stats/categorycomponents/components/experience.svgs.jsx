554f47e5c98991e7c0dbc77597b54c1d20705bf2
Aaron Cheng <acheng8@lsu.edu>
2023-03-31 18:27:41 -0500

    Experience

--- src/routes/vita/stats/categorycomponents/components/experience.svgs.jsx
+++ src/routes/vita/stats/categorycomponents/components/experience.svgs.jsx
@@ -3,22 +3,34 @@ export const experience_svgs = {
     image: (
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/LSU_Athletics_logo.svg/1024px-LSU_Athletics_logo.svg.png"
+        className="LSU-Image"
         alt="LSU symbol"
       />
     ),
   },
   BRMHS: {
     image: (
-      <img src="https://i.ibb.co/jH74Mnr/bulldog.png" alt="BRMHS symbol" />
+      <img
+        src="https://i.ibb.co/jH74Mnr/bulldog.png"
+        className="BRMHS-Image"
+        alt="BRMHS symbol"
+      />
     ),
   },
   ARA: {
-    image: <img src="https://i.ibb.co/47vpDqn/ARALogo.png" alt="ARA symbol" />,
+    image: (
+      <img
+        src="https://i.ibb.co/47vpDqn/ARALogo.png"
+        className="ARA-Image"
+        alt="ARA symbol"
+      />
+    ),
   },
   Exodus: {
     image: (
       <img
         src="https://i.ibb.co/tzPWrqw/Tight-Exodus-Only.png"
+        className="Exodus-Image"
         alt="Exodus symbol"
       />
     ),
@@ -27,6 +39,7 @@ export const experience_svgs = {
     image: (
       <img
         src="https://img.icons8.com/cotton/64/null/sushi.png"
+        className="Mini-Image"
         alt="Sushi symbol"
       />
     ),
@@ -35,6 +48,7 @@ export const experience_svgs = {
     image: (
       <img
         src="https://img.icons8.com/external-smashingstocks-isometric-smashing-stocks/55/null/external-customer-service-social-media-smashingstocks-isometric-smashing-stocks.png"
+        className="Mini-Image"
         alt="Grocery symbol"
       />
     ),
@@ -43,6 +57,7 @@ export const experience_svgs = {
     image: (
       <img
         src="https://img.icons8.com/fluency/96/null/bubble-tea-.png"
+        className="Mini-Image"
         alt="Boba symbol"
       />
     ),
--- src/routes/vita/stats/categorycomponents/experience.component.jsx
+++ src/routes/vita/stats/categorycomponents/experience.component.jsx
@@ -7,18 +7,43 @@ export const Experience = () => {
     <Fragment>
       <div className="Experience-Container">
         <label className="Time">Currently...</label>
-        <div className="Experience-Block">
-          <div className="Image-Container">{experience_svgs.ARA.image}</div>
+
+        <div className="College-Container">
+          <div className="College-Jobs">
+            <div className="Experience-Block">
+              <div className="Image-Container">{experience_svgs.ARA.image}</div>
+            </div>
+            <div className="Experience-Block">
+              <div className="Image-Container">
+                {experience_svgs.Exodus.image}
+              </div>
+            </div>
+          </div>
+          <div className="Experience-Block-LSU">
+            <div className="Image-Container">{experience_svgs.LSU.image}</div>
+          </div>
         </div>
-        <div className="Experience-Block">
-          <div className="Image-Container">{experience_svgs.Exodus.image}</div>
-        </div>
-        <div className="Experience-Block">
-          <div className="Image-Container">{experience_svgs.LSU.image}</div>
-        </div>
+
         <label className="Time">In the past...</label>
-        <div className="Experience-Block">
-          <div className="Image-Container">{experience_svgs.ARA.image}</div>
+        <div className="College-Container">
+          <div className="Experience-Block-HS">
+            <div className="Image-Container">{experience_svgs.BRMHS.image}</div>
+          </div>
+          <div className="College-Jobs">
+            <div className="Experience-Block">
+              <div className="Image-Container">
+                {experience_svgs.Sushi.image}
+              </div>
+            </div>
+            <div className="Experience-Block">
+              <div className="Image-Container">{experience_svgs.Tea.image}</div>
+            </div>
+            <div className="Experience-Block">
+              <div className="Image-Container">
+                {experience_svgs.Grocery.image}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
