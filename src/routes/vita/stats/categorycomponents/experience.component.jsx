288abdc39f6f236cd5fc7047fee1c29b31b7a933
Aaron Cheng <acheng8@lsu.edu>
2023-03-30 22:55:26 -0500

    Experience styling

--- src/routes/vita/stats/categorycomponents/experience.component.jsx
+++ src/routes/vita/stats/categorycomponents/experience.component.jsx
@@ -6,7 +6,20 @@ export const Experience = () => {
   return (
     <Fragment>
       <div className="Experience-Container">
-        <div className="Experience-Block">{experience_svgs.ARA.image}</div>
+        <label className="Time">Currently...</label>
+        <div className="Experience-Block">
+          <div className="Image-Container">{experience_svgs.ARA.image}</div>
+        </div>
+        <div className="Experience-Block">
+          <div className="Image-Container">{experience_svgs.Exodus.image}</div>
+        </div>
+        <div className="Experience-Block">
+          <div className="Image-Container">{experience_svgs.LSU.image}</div>
+        </div>
+        <label className="Time">In the past...</label>
+        <div className="Experience-Block">
+          <div className="Image-Container">{experience_svgs.ARA.image}</div>
+        </div>
       </div>
     </Fragment>
   );
