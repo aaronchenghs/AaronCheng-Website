95de31585da1de0aa1cfd8ac7e2e289bd06996c7
aaronchenghs <aaronchenghs@gmail.com>
2023-04-13 13:05:31 -0500

    I got a google logo

--- src/routes/feedback/feedback.component.jsx
+++ src/routes/feedback/feedback.component.jsx
@@ -11,7 +11,13 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import "./feedback.styles.scss";
-
+const GoogleLogo = (
+  <img
+    width="30"
+    alt='Google "G" Logo'
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+  />
+);
 const Feedback = () => {
   const dispatch = useDispatch();
 
@@ -41,6 +47,7 @@ const Feedback = () => {
               <FeedbackInput />
             ) : (
               <button className="sign-in-button" onClick={logGoogleUser}>
+                {GoogleLogo}
                 Sign in with Google to leave feedback
               </button>
             )
