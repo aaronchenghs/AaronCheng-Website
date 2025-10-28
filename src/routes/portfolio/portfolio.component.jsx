ab311dd467ec9139f21994c2a06197b7ca13b947
Aaron Cheng <acheng8@lsu.edu>
2025-10-27 21:33:55 -0500

    Swap certs with projects

--- src/routes/portfolio/portfolio.component.jsx
+++ src/routes/portfolio/portfolio.component.jsx
@@ -10,17 +10,16 @@ const Portfolio = () => {
   return (
     <Fragment>
       <div className="Portfolio-Page container py-4">
-        <PageHeader text={"My Projects 📦"} />
-        <div className="cards-container">
-          {projects.map((project) => (
-            <Project project={project} key={project.alt} />
-          ))}
-        </div>
-
         <PageHeader text={"My Certifications ✅"} />
         <div className="certs-container">
           {certs.map((cert) => (
             <CredlyBadge key={cert.id} cert={cert} />
+          ))}
+        </div>
+        <PageHeader text={"My Projects 📦"} />
+        <div className="cards-container">
+          {projects.map((project) => (
+            <Project project={project} key={project.alt} />
           ))}
         </div>
       </div>
